84cfa52c38a1d063ad6248f7c382b521b6a8fd2f
Justin Taras <taras.justin@gmail.com>
2022-09-22 11:27:27 -0400

    initial

---
+++ datafusion-load-plugins/main.tf
@@ -0,0 +1,27 @@
+data "google_client_config" "current" {
+}
+
+provider "cdap" {
+  host  = "https://jtaras2-generalproject-340815-dot-usc1.datafusion.googleusercontent.com/api/"
+  token = data.google_client_config.current.access_token
+}
+
+module "Teradata_1_7_0" {
+  source = "./modules/hub_artifact"
+
+  bucket           = "cdfhub-us-central1/hub"
+  package          = "database-plugin-teradata-plugin"
+  name             = "teradata-plugin"
+  artifact_version = "1.7.0"
+  namespace        = "DG"
+}
+
+module "Netezza_1_7_0" {
+  source = "./modules/hub_artifact"
+
+  bucket           = "cdfhub-us-central1/hub"
+  package          = "database-plugin-netezza-plugin"
+  name             = "netezza-plugin"
+  artifact_version = "1.7.0"
+  namespace        = "DG"
+}
